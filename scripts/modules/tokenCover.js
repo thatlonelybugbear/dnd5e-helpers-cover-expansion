17eedbada2e3fa9f9815db3b2bc8c22e0d2be1b4
thatlonelybugbear <idonotwantspams@gmail.com>
2022-08-17 12:10:57 +0100

    Update tokenCover.js
    Expand tokenCover to apply on Prone and Unconscious.

--- scripts/modules/tokenCover.js
+++ scripts/modules/tokenCover.js
@@ -101,6 +101,7 @@ function setting(key) {
 }
 
 Hooks.on("preUpdateActor", async (actor, update) => {
+    if (!actor.token || !actor.getActiveTokens()[0]?.document) return;
     let hp = getProperty(update, "data.attributes.hp.value");
     let token = actor.token ?? actor.getActiveTokens()[0].document
     
@@ -113,3 +114,31 @@ Hooks.on("preUpdateActor", async (actor, update) => {
         await token.setFlag("dnd5e-helpers", "coverLevel", cover)
     }
 })
+
+Hooks.on("preCreateActiveEffect", async (effect) => {
+    if (!effect.parent.parent || !effect.parent.getActiveTokens()[0]?.document) return;
+    let label = effect.data.label;
+    let token = effect.parent.parent ?? effect.parent.getActiveTokens()[0].document;
+    let actor = token.actor;
+    //let currentCover = foundry.utils.getProperty(token.data.flags, "dnd5e-helpers.coverLevel")
+    if (label === "Unconscious" || label === "Prone") {
+        let cover = tokenCover.coverValue(actor, "dead");
+        await token.setFlag("dnd5e-helpers", "coverLevel", cover);
+    }
+    /*else if(label === "Prone" && !token.actor.effects.find(eff=>eff.data.label === "Unconscious")) {
+        let cover = tokenCover.coverValue(actor, "prone")
+        await token.setFlag("dnd5e-helpers", "coverLevel", cover)
+    }*/
+});
+
+Hooks.on("preDeleteActiveEffect", async (effect) => {
+    if (!effect.parent.parent || !effect.parent.getActiveTokens()[0]?.document) return;
+    let label = effect.data.label;
+    let token = effect.parent.parent ?? effect.parent.getActiveTokens()[0].document;
+    let actor = token.actor;
+    if ((label === "Unconscious" && !token.actor.effects.find(eff=>eff.data.label === "Prone") && !token.actor.getRollData().attributes.hp.value === 0) || (label === "Prone" && !token.actor.effects.find(eff=>eff.data.label === "Unconscious") && !token.actor.getRollData().attributes.hp.value === 0)) {
+        let cover = tokenCover.coverValue(actor, "cover")
+        await token.setFlag("dnd5e-helpers", "coverLevel", cover)
+    }
+});
+    
